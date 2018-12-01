3145dc004a4ac9960df04ee3361eb05dd0c55804
Kelly Galakatos <kellygalakatos@kellys-macbook-air-2.local>
2018-11-30 20:14:51 -0600

    Initial Commit

---
+++ MachineLearningVision/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MachineLearningVision
+//
+//  Created by Kelly Galakatos on 11/30/18.
+//  Copyright © 2018 Kelly Galakatos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
